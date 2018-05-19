4d5351db76a0e2d6566e44f298a76e47354cd82e
nelson mao <18851733010@163.com>
2018-05-19 11:09:11 +0800

    Create 4g.rand.cpp

---
+++ program/4g.rand.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+    char a[20];
+    int i,j;
+    //利用ofstream类的构造函数创建一个文件输出流对象来打开文件 
+    ofstream fout( "/home/darker/4G.txt" );  
+    if ( ! fout){
+        cout << "文件不能打开" <<endl;
+    }
+    else{
+        while(1){
+            j=rand()%(15-1+1)+1;
+        for(i=0;i<j;i++){
+            if (rand()%2==1){
+                a[i]=rand()%('z'-'a'+1)+'a';
+            }
+            else {
+                a[i]=rand()%('Z'-'A'+1)+'A';
+            }
+        }
+        a[++i]='\n';
+        fout << a << endl; 
+        }
+    }
+}
